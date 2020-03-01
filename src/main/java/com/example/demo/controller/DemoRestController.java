9e10d706dcb853427986e5871cba750de394b194
beerbauf <cirillo.ndr@gmail.com>
2020-03-01 15:48:51 +0100

    aggiunto msg

--- src/main/java/com/example/demo/controller/DemoRestController.java
+++ src/main/java/com/example/demo/controller/DemoRestController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoRestController {
 
+    @Value("${user.msg}")
+    private String userMessage;
+
     @GetMapping(value = "/hello")
     public String getHello(){
-        return "Hello World!";
+        return "[ "+userMessage +" ]";
     }
 }
